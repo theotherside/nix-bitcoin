2f6cfb3e8c9136d9949b01bfc5812b143c01c0f8
nixbitcoin <nixbitcoin@i2pmail.org>
2020-04-19 18:10:21 +0200

    Update pinned nixpkgs with fetchurl

--- pkgs/nixpkgs-pinned.nix
+++ pkgs/nixpkgs-pinned.nix
@@ -1,18 +1,25 @@
 let
   fetch = { rev, sha256 }:
-    builtins.fetchTarball {
+    builtins.fetchurl {
       url = "https://github.com/nixos/nixpkgs-channels/archive/${rev}.tar.gz";
       inherit sha256;
     };
 in
-{
-  # To update, run ../helper/fetch-channel REV
-  nixpkgs = fetch {
-    rev = "95b9c99f6d091273572ff1ec62b97b6ad3f68bdf";
-    sha256 = "1qcwr9binkwfayix88aljwssxi5djkwscx0rnwlk1yp9q60rgp3d";
+rec {
+  # To update, run ../helper/wot-update-channels
+  nixpkgs-packed = fetch {
+    rev = "1d8a149ccea76b6fed87d2506391c2905a4c0440";
+    sha256 = "60e0a600776e0548e5540d83bffb8c958794a2057d5b0f7362113344bffbe0a7";
   };
-  nixpkgs-unstable = fetch {
-    rev = "22a3bf9fb9edad917fb6cd1066d58b5e426ee975";
-    sha256 = "089hqg2r2ar5piw9q5z3iv0qbmfjc4rl5wkx9z16aqnlras72zsa";
+  nixpkgs-unstable-packed = fetch {
+    rev = "a7971df962fd026843f1707c237294341920757e";
+    sha256 = "a5c40ca1a4a595cfea3596045c6da2db9556e3dd680e92123e45f848c36a578e";
   };
+
+  nixpkgs = (import <nixpkgs> {}).runCommand "nixpkgs-src" {} ''
+    mkdir $out; tar xf "${toString nixpkgs-packed }" --strip 1 -C $out
+  '';
+  nixpkgs-unstable = (import <nixpkgs> {}).runCommand "nixpkgs-unstable-src" {} ''
+    mkdir $out; tar xf "${toString nixpkgs-unstable-packed }" --strip 1 -C $out
+  '';
 }
